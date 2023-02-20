b9d0eb86d459e7d3f32462ffa9b562cef421e074
Mateus Sousa <sousa.programador@gmail.com>
2023-02-20 10:47:15 -0300

    new user cases

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,19 +1,19 @@
-import { Response,Request, request } from "express";
-import { container } from "tsyringe";
-import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
+import { Response, Request, request } from 'express';
+import { container } from 'tsyringe';
+
+import { UpdateUserAvatarUseCase } from './UpdateUserAvatarUseCase';
 
 class UpdateUserAvatarController {
-
-  async handle(resquest:Request,response:Response):Promise<Response>{
+  async handle(resquest: Request, response: Response): Promise<Response> {
     const { id } = request.user;
     const avatar_file = request.file.filename;
 
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
-    await updateUserAvatarUseCase.execute({ user_id:id,avatar_file });
+    await updateUserAvatarUseCase.execute({ user_id: id, avatar_file });
 
-    return response.send(204);
+    return response.status(204).send();
   }
 }
 
-export { UpdateUserAvatarController }
+export { UpdateUserAvatarController };
