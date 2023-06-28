c718eebb83e92b0c57a81b24e5749154f0472052
Mateus Sousa <sousa.programador@gmail.com>
2023-06-28 09:31:06 -0300

    add nest framework

--- src/shared/container/providers/DateProvider/index.ts
+++
@@ -1,9 +0,0 @@
-import { container } from 'tsyringe';
-
-import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
-import { DayjsDateProvider } from '@shared/container/providers/DateProvider/implementations/DayjsDateProvider';
-
-container.registerSingleton<IDateProvider>(
-  'DayjsDateProvider',
-  DayjsDateProvider,
-);
