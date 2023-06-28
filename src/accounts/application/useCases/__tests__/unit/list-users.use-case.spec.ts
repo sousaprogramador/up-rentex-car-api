9b7eeab2995a08175f23e70fe3a59c0454953294
Mateus Sousa <sousa.programador@gmail.com>
2023-06-28 17:08:06 -0300

    List User cases

---
+++ src/accounts/application/useCases/__tests__/unit/list-users.use-case.spec.ts
@@ -0,0 +1,94 @@
+import ListUsersUseCase from '../../list-users.use-case';
+import UserInMemoryRepository from '../../../../infra/db/in-memory/user-in-memory.repository';
+import { User } from '../../../../domain/entities';
+import UserRepository from '../../../../domain/repository/user.repository';
+
+describe('ListUsersUseCase Unit Tests', () => {
+  let useCase: ListUsersUseCase.UseCase;
+  let repository: UserInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new UserInMemoryRepository();
+    useCase = new ListUsersUseCase.UseCase(repository);
+  });
+
+  test('toOutput method', () => {
+    let result = new UserRepository.SearchResult({
+      items: [],
+      total: 1,
+      current_page: 1,
+      per_page: 2,
+      sort: null,
+      sort_dir: null,
+      filter: null,
+    });
+    let output = useCase['toOutput'](result);
+    expect(output).toStrictEqual({
+      items: [],
+      total: 1,
+      current_page: 1,
+      per_page: 2,
+      last_page: 1,
+    });
+
+    const entity = new User({
+      name: 'some test',
+      email: 'sometest@mail.com',
+      password: '1234',
+      driver_licenses: '1234',
+      is_active: true,
+      avatar: null,
+    });
+
+    result = new UserRepository.SearchResult({
+      items: [entity],
+      total: 1,
+      current_page: 1,
+      per_page: 2,
+      sort: null,
+      sort_dir: null,
+      filter: null,
+    });
+
+    output = useCase['toOutput'](result);
+    expect(output).toStrictEqual({
+      items: [entity.toJSON()],
+      total: 1,
+      current_page: 1,
+      per_page: 2,
+      last_page: 1,
+    });
+  });
+
+  it('should return output sorted by created_at when input param is empty', async () => {
+    const items = [
+      new User({
+        name: 'some test',
+        email: 'sometest@mail.com',
+        password: '1234',
+        driver_licenses: '1234',
+        is_active: true,
+        avatar: null,
+      }),
+      new User({
+        name: 'some test 2',
+        email: 'sometest2@mail.com',
+        password: '1234',
+        driver_licenses: '1234',
+        is_active: false,
+        avatar: null,
+        created_at: new Date(new Date().getTime() + 100),
+      }),
+    ];
+    repository.items = items;
+
+    const output = await useCase.execute({});
+    expect(output).toStrictEqual({
+      items: [...items].reverse().map((i) => i.toJSON()),
+      total: 2,
+      current_page: 1,
+      per_page: 15,
+      last_page: 1,
+    });
+  });
+});
---
+++ src/accounts/application/useCases/list-users.use-case.ts
@@ -0,0 +1,33 @@
+import UserRepository from '../../domain/repository/user.repository';
+import { UserOutput, UserOutputMapper } from '../dto/user-output';
+import { default as DefaultUseCase } from '../../../@seedwork/application/use-cases';
+import { SearchInputDto } from '../../../@seedwork/application';
+import {
+  PaginationOutputDto,
+  PaginationOutputMapper,
+} from '../../../@seedwork/application';
+
+export namespace ListUsersUseCase {
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private userRepo: UserRepository.Repository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const params = new UserRepository.SearchParams(input);
+      const searchResult = await this.userRepo.search(params);
+      return this.toOutput(searchResult);
+    }
+
+    private toOutput(searchResult: UserRepository.SearchResult): Output {
+      const { items: _items } = searchResult;
+      const items = _items.map((i) => {
+        return UserOutputMapper.toOutput(i);
+      });
+      return PaginationOutputMapper.toOutput(items, searchResult);
+    }
+  }
+
+  export type Input = SearchInputDto;
+
+  export type Output = PaginationOutputDto<UserOutput>;
+}
+export default ListUsersUseCase;
