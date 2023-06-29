6b4083f551bad8f1b08062de33f85ed350987da5
Mateus Sousa <sousa.programador@gmail.com>
2023-06-29 17:37:14 -0300

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './user/infra/nest/user.module';
 import { seedworkModule } from './@seedwork/infra/nest/@seedwork.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [AccountsModule, seedworkModule],
+  imports: [AccountsModule, seedworkModule, DatabaseModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
