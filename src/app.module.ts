f8c04354ad028d58b5f395d24ddf274008c40848
Mateus Sousa <sousa.programador@gmail.com>
2023-06-29 09:32:00 -0300

    add presentation in seedwork module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/infra/nest/accounts.module';
+import { seedworkModule } from './@seedwork/infra/nest/@seedwork.module';
 
 @Module({
-  imports: [AccountsModule],
+  imports: [AccountsModule, seedworkModule],
   controllers: [AppController],
   providers: [AppService],
 })
