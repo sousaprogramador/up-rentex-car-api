20621bc8b7557d8744338c39a7a6497df15c4cf5
Mateus Sousa <sousa.programador@gmail.com>
2023-09-23 13:49:39 -0300

    add storage config

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 module.exports = {
-  host: 'localhost',
-  port: 33006,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
